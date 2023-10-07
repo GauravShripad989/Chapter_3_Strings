c2ba9359005d17941140db8486a56bd0b6a4ecea
GauravShripad989 <gauravshripad127@gmail.com>
2023-10-08 05:26:56 +0530

    Some changes in Intro_String.java Class

--- Intro_String.java
+++ Intro_String.java
@@ -1,4 +1,5 @@
 package Chapter_3_String.Chapter_3_Strings;
+import java.util.Scanner;
 
 public class Intro_String {
     public static void main(String[] args) {
@@ -7,5 +8,12 @@ public class Intro_String {
         System.out.println(name + " " + last_Name);//String are immutable means you cannot
                                                    //change the same String by any property
                                                    //just make a copy and do it
+        int a = 50;
+        float b = 23.36548f;
+        System.out.printf("value of a is %d and value of b is %f \n", a, b);
+
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.nextLine();
+        System.out.format(userName);
     }
 }
